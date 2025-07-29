f2711c12ddfb25476801731d80084ee80f945053
rjmlaird <rjmlaird@gmail.com>
2025-07-29 18:53:47 +0100

    cookies & GA

---
+++ src/scripts/initGA.js
@@ -0,0 +1,20 @@
+export function initGA() {
+  if (typeof window.gtag === 'function') return;
+
+  const GA_ID = "G-411653436";
+
+  const script1 = document.createElement("script");
+  script1.async = true;
+  script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+
+  const script2 = document.createElement("script");
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${GA_ID}', { anonymize_ip: true });
+  `;
+
+  document.head.appendChild(script1);
+  document.head.appendChild(script2);
+}
